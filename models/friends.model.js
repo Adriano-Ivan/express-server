fafb6be9aa350a89ddafd9842c0fe9528dbf9dc8
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-02-20 20:16:04 -0300

    Trabalhando conceitos de MVC.

---
+++ models/friends.model.js
@@ -0,0 +1,9 @@
+const friends = [
+  { id: 0, name: "Sir William Golding" },
+  { id: 1, name: "Sir Roger Scruton" },
+  { id: 2, name: "Fiódor Dostoiévski" },
+];
+
+module.exports = {
+  friends,
+};
--- server.js
+++ server.js
@@ -1,45 +1,27 @@
 const express = require("express");
+const messagesController = require("./controllers/messages.controller");
+const friendsController = require("./controllers/friends.controller");
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-  { id: 1, name: "Sir William Golding" },
-  { id: 2, name: "Sir Roger Scruton" },
-  { id: 3, name: "Fiódor Dostoiévski" },
-];
-
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  // console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
-app.post();
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
+app.use(express.json());
 
-app.get("/friends/:id", (req, res) => {
-  const id = req.params.id;
-  const friend = friends.find((f) => f.id === parseInt(id));
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    req.status(404).json({
-      error: "Friend does not exist",
-    });
-  }
-});
-app.get("/messages", (req, res) => {
-  res.send("<ul><li>Hello Mozart !</li></ul>");
-});
+app.post("/friends", friendsController.postFriend);
+app.get("/friends", friendsController.getFriends);
+app.get("/friends/:id", friendsController.getFriendById);
 
-app.post("/messages", (req, res) => {
-  console.log("Updating messages...");
-});
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessages);
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
